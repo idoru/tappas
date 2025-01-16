b2b819a9364fd54e329732e9e041b44a468a7082
Sam Coward <idoru42@gmail.com>
2025-01-15 21:04:00 -0500

    crude rebuild

---
+++ iterate.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+export TAPPAS_WORKSPACE=$(pwd)
+export CAMSERV_WORKSPACE=$TAPPAS_WORKSPACE/../camserv
+
+repave() {
+  pushd "$TAPPAS_WORKSPACE"
+  ./scripts/gstreamer/install_hailo_gstreamer.sh --skip-hailort
+  popd
+
+  pushd "$CAMSERV_WORKSPACE"
+  make clean
+  make all
+  sudo make install
+  sudo systemctl restart webcam.service
+  popd
+}
+
+if [ "$1" = "--repave" ]; then
+  repave
+  exit 0
+fi
+
+set -exuo pipefail
+find . -type f | entr -s "$0 --repave"
